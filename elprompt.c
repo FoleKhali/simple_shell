c651b8277d0fb4c81ecb57bc2b32ffb706c85895
FoleKhali <platanobrgas@gmail.com>
2022-04-06 00:28:23 -0500

    first phase of the prompt

---
+++ elprompt.c
@@ -0,0 +1,31 @@
+#include "sshell.h"
+
+/**
+ * main - make a prompt that starts to read cmd (commands)
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *cmd;
+
+	cmd = NULL;
+	size_t  size = 0;
+	ssize_t read_b = 0;
+
+	while (1)
+	{
+		printf("#cisfun$ ");
+		read_b = getline(&cmd, &size, stdin);
+		if (strcmp(cmd, "exit\n") == 0)
+		{
+			if (cmd)
+				printf("Successful exit!\n");
+			free(cmd);
+			exit(0);
+		}
+		cmd[strlen(cmd) - 1] = '\0';
+		execute_cmd(cmd);
+	}
+	return (0);
+}
