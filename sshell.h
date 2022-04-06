c77cc64879191be4b459a6f78a2dab949c1904e8
JSM788 <salinasjuan788@gmail.com>
2022-04-06 01:02:10 -0500

    New header1

---
+++ sshell.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/wait.h>
+
+void execute_cmd(char *cmd);
+#endif
+~                                                                                                                                         ~
