17f813b201d135eb035c4418529f060616c1c7ec
JSM788 <salinasjuan788@gmail.com>
2022-04-06 00:51:47 -0500

    New Header

--- main.h
+++ main.h
@@ -1,4 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void execute_cmd(char *cmd);
 #endif
