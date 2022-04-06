d6c2e628c44bfbc971a9c9e9120452195819907a
JSM788 <salinasjuan788@gmail.com>
2022-04-06 01:01:11 -0500

    Execute function

---
+++ execute_cmd.c
@@ -0,0 +1,32 @@
+#include "sshell.h"
+
+/*
+ * execute_cmd - helps execute commands from the prompt
+ * @cmd: pointer
+ */
+
+void execute_cmd(char *cmd)
+{
+	pid_t pid = 0;
+	int child_status = 0;
+	char *instruction[] = {cmd, NULL};
+
+	pid = fork();
+	if (pid == -1)
+	{
+		exit(0);
+	}
+	else if (pid == 0)
+	{
+		if (access(cmd, F_OK) == 0)
+			execve(instruction[0], instruction, NULL);
+		else
+			perror("./sHell");
+			free(cmd);
+			exit(1);
+	}
+	else
+	{
+		wait(&child_status);
+	}
+}
