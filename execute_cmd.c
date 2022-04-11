6671245fa33c765a9ae72831dc620c7dc8acbb52
Juan Salinas <salinasjuan788@gmail.com>
2022-04-11 00:54:05 -0700

    new changes2

--- execute_cmd.c
+++ execute_cmd.c
@@ -1,38 +1,41 @@
 #include "sshell.h"
-
+/**
+ * execute_cmd - check if the function works correctly
+ * @cmd: pointer
+ */
 
 void execute_cmd(char *cmd)
 {
-        pid_t pid = 0;
-        int child_status = 0;
-        char *path_bin[2];
-        path_bin[0] = cmd;
-        path_bin[1] = NULL;
+	pid_t pid = 0;
+	int child_status = 0;
+	char *path_bin[2];
 
-        pid = fork();
+	path_bin[0] = cmd;
+	path_bin[1] = NULL;
 
-        if (pid == -1)
-        {
-                perror("./sHell");
-                free(cmd);
-                exit(0);
-        }
-        else if (pid == 0)
-        {
-        /*execve ejecuta*/
-                if (access(cmd, F_OK) == 0)
-                        execve(path_bin[0], path_bin, NULL);
+	pid = fork();
 
-                else
-                {
-                        perror("./sHell");
-                        free(cmd);
-                        exit(1);
-                }
-        }
-        else
-        {
-                wait(&child_status);
-        }
+	if (pid == -1)
+	{
+		perror("./sHell");
+		free(cmd);
+		exit(0);
+	}
+	else if (pid == 0)
+	{
+	/*execve ejecuta*/
+		if (access(cmd, F_OK) == 0)
+			execve(path_bin[0], path_bin, NULL);
+
+		else
+		{
+			perror("./sHell");
+			free(cmd);
+			exit(1);
+		}
+	}
+	else
+	{
+		wait(&child_status);
+	}
 }
-
