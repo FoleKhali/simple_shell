502fa3dcfe21b5b6bc9caf90d5b8415183d231ef
FoleKhali <platanobrgas@gmail.com>
2022-04-11 16:32:33 -0500

    betty

--- new_defuncions.c
+++ new_defuncions.c
@@ -1,27 +1,32 @@
 #include "sshell.h"
-
+/**
+ * the_exit - built to exit
+ * @cmd: command
+ * Return: nothing
+ */
 void the_exit(char *cmd)
 {
-        if (_strcmp(cmd, "exit\n") == 0)
-        {
-                        printf("Successful exit!\n");
-                        free(cmd);
-                        exit(0);
-        }
+	if (_strcmp(cmd, "exit\n") == 0)
+	{
+		free(cmd);
+		exit(0);
+	}
 }
-
-
+/**
+ * the_env - find enviromment variable
+ * @cmd: command
+ * Return: nothing
+ */
 void the_env(char *cmd)
 {
-        int c = 0;
+	int c = 0;
 
-        if (_strcmp(cmd, "env\n") == 0)
-        {
-                while(environ[c] != NULL)
-                {
-                        printf("[%s] \n ", environ[c]);
-                        c++;
-                }
-        }
+	if (_strcmp(cmd, "env\n") == 0)
+	{
+		while (environ[c] != NULL)
+		{
+			printf("[%s] \n ", environ[c]);
+			c++;
+		}
+	}
 }
-
