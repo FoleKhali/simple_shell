62e494eb256f60ec9edf433109beb731a059dd32
Juan Salinas <salinasjuan788@gmail.com>
2022-04-11 21:31:55 -0700

    New header

--- sshell.h
+++ sshell.h
@@ -18,19 +18,15 @@ extern char **environ;
 #include <signal.h>
 
 /*execute_cmd*/
-void execute_cmd(char *cmd);
+void execute_cmd(char *tokens);
 /*exit shell*/
-int the_exit(char *cmd);
+int the_exit(char *tokens);
 /*get env*/
-int the_env(char *cmd);
+int the_env(char *tokens);
 /*tokenize the cmd*/
 char tokenizer(char *cmd);
 /* functions in defuncion.c */
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
-
-/*pruebas*/
-int token(char *cmd);
-
 
 #endif
