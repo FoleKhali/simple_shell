5c5ed8ffb72bcdc9defa86d080e49642e26eef21
FoleKhali <platanobrgas@gmail.com>
2022-04-12 01:00:23 -0500

    betty

--- sHell1.c
+++ sHell1.c
@@ -7,34 +7,33 @@
 
 int main(void)
 {
-        ssize_t read_cmd = 0;
-        size_t size_bytes = 0;
-        char *cmd = NULL;
-        char **tokens;
-        int i = 0;
+	ssize_t read_cmd = 0;
+	size_t size_bytes = 0;
+	char *cmd = NULL;
+	char **tokens;
 
-        signal(SIGINT, SIG_IGN);
+	signal(SIGINT, SIG_IGN);
 
-        while (1)
-        {
-                if (isatty(STDIN_FILENO) == 1)
-                        printf("#Cisfun$ ");
-                read_cmd = getline(&cmd, &size_bytes, stdin);
+	while (1)
+	{
+		if (isatty(STDIN_FILENO) == 1)
+			printf("#Cisfun$ ");
+		read_cmd = getline(&cmd, &size_bytes, stdin);
 
-                /*end of file*/
-                if (read_cmd == EOF)
-                {
-                        printf("\nBye bye!\n");
-                        free(cmd);
-                        exit(0);
-                }
-                if (read_cmd == 1)
-                {
-                        continue;
-                }
-                tokens = tokenizer(cmd);
-          the_env(tokens),the_exit(tokens);
-                        execute_cmd(tokens);
-        }
-        return (0);
+		/*end of file*/
+		if (read_cmd == EOF)
+		{
+			printf("\nBye bye!\n");
+			free(cmd);
+			exit(0);
+		}
+		if (read_cmd == 1)
+		{
+			continue;
+		}
+		tokens = tokenizer(cmd);
+		the_env(tokens), the_exit(tokens);
+		execute_cmd(tokens);
+	}
+	return (0);
 }
